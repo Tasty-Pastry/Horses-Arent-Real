8bcc2cb2979757dbb43678d7d9ba38c074c0333d
Tasty-Pastry <Tasty-Pastry@users.noreply.github.com>
2019-01-15 13:08:35 -0500

    S p a c ing
    Fix spacing u hecks

--- src/items/TeethButter.java
+++ src/items/TeethButter.java
@@ -6,74 +6,75 @@ import java.awt.image.BufferedImage;
 
 import animation.BufferedImageLoader;
 import entity.GameObject;
-import game.ID;
 import game.Game;
+import game.ID;
 
 public class TeethButter extends GameObject {
 
 	private BufferedImageLoader loader;
+	private boolean vored;
 
 	public TeethButter(int x, int y, ID id) {
 
 		super(x, y, id);
 
 		name = "Teeth Butter";
-		
-		lore="...Yikes.";
-		
-		itemDesc="Eat this to regain 15% of your HP.";
+
+		lore = "...Yikes.";
+
+		itemDesc = "Eat this to regain 15% of your HP.";
 
 		loader = new BufferedImageLoader();
 
 		sprite = loader.loadImage("/TeethButter.png");
-		
+
 		keyItem = false;
-		passive=false;
-		vored=false;
+		passive = false;
+		vored = false;
 
 	}
-	
+
 	public void update() {
 
 	}
-	
+
 	public void use() {
-		
-		if (Game.getCharacter()==1) {
-			
-			if (Game.daanishHealth>193)
-				Game.daanishHealth=228;
+
+		if (Game.getCharacter() == 1) {
+
+			if (Game.daanishHealth > 193)
+				Game.daanishHealth = 228;
 			else
-				Game.daanishHealth+=(int)(228*0.15);
-			
+				Game.daanishHealth += (int) (228 * 0.15);
+
 		}
-			
-		else if (Game.getCharacter()==2) {
-			
-			if (Game.nickHealth>193)
-				Game.nickHealth=228;
+
+		else if (Game.getCharacter() == 2) {
+
+			if (Game.nickHealth > 193)
+				Game.nickHealth = 228;
 			else
-				Game.nickHealth+=(int)(228*0.15);
-			
+				Game.nickHealth += (int) (228 * 0.15);
+
 		}
-		
+
 		else {
-			
-			if (Game.namelessHealth>193)
-				Game.namelessHealth=228;
+
+			if (Game.namelessHealth > 193)
+				Game.namelessHealth = 228;
 			else
-				Game.namelessHealth+=(int)(228*0.15);
-			
+				Game.namelessHealth += (int) (228 * 0.15);
+
 		}
-		
-		vored=true;
-		
+
+		vored = true;
+
 	}
 
 	// Draw crate
 	public void draw(Graphics g) {
 
-		g.drawImage(sprite,x,y,null);
+		g.drawImage(sprite, x, y, null);
 
 	}
 
@@ -83,7 +84,6 @@ public class TeethButter extends GameObject {
 		return new Rectangle(x, y, 32, 32);
 
 	}
-
 
 	@Override
 	public String getName() {
@@ -97,5 +97,5 @@ public class TeethButter extends GameObject {
 
 		return sprite;
 	}
-	
+
 }
